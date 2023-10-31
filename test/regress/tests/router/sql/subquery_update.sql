949c230fc365b20372243cf35e82a4233fb8bb61
Alexander Khimulya <viscount@yandex-team.ru>
2023-10-31 16:17:37 +0100

    test for update with subquery

---
+++ test/regress/tests/router/sql/subquery_update.sql
@@ -0,0 +1,20 @@
+\c spqr-console
+CREATE SHARDING RULE r1 COLUMN i;
+CREATE KEY RANGE kridi1 from 0 to 11 route to sh1;
+CREATE KEY RANGE kridi2 from 11 to 21 route to sh2;
+
+\c regress
+
+CREATE TABLE tbl(id int, i int);
+
+INSERT INTO tbl (id, i) VALUES(1, 1);
+
+INSERT INTO tbl (id, i) VALUES(12, 12);
+INSERT INTO tbl (id, i) VALUES(13, 12);
+
+update tbl set id = id + 1 where i = 12 and id in (select id from tbl where i = 12) returning *;
+
+DROP TABLE tbl;
+\c spqr-console
+DROP KEY RANGE ALL;
+DROP SHARDING RULE ALL;
